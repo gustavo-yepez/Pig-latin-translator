84de40ec9c1f50ccc0914a227ba5a1feec58361c
gustavo yepez <gustavoyepez96@gmail.com>
2022-04-23 23:23:36 -0400

    Initial commit

---
+++ Pig Latin/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Pig_Latin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionsLabel = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.close_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // instructionsLabel
+            // 
+            this.instructionsLabel.AutoSize = true;
+            this.instructionsLabel.Location = new System.Drawing.Point(196, 46);
+            this.instructionsLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.instructionsLabel.Name = "instructionsLabel";
+            this.instructionsLabel.Size = new System.Drawing.Size(173, 13);
+            this.instructionsLabel.TabIndex = 0;
+            this.instructionsLabel.Text = "Enter a string of letters and spaces.";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(91, 95);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(390, 20);
+            this.inputTextBox.TabIndex = 1;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(199, 160);
+            this.convertButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(92, 43);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // close_btn
+            // 
+            this.close_btn.Location = new System.Drawing.Point(296, 160);
+            this.close_btn.Name = "close_btn";
+            this.close_btn.Size = new System.Drawing.Size(88, 43);
+            this.close_btn.TabIndex = 3;
+            this.close_btn.Text = "Close";
+            this.close_btn.UseVisualStyleBackColor = true;
+            this.close_btn.Click += new System.EventHandler(this.close_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 270);
+            this.Controls.Add(this.close_btn);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.instructionsLabel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Pig Latin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button close_btn;
+    }
+}
+
